b92aa256a47660c7ec7415d3e269217a834ec36b
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-11-16 13:47:45 -0500

    added mongoose models

---
+++ server/server.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: 'len@test.com'
+});
+
+newUser.save().then((res) => {
+  console.log('User created', res);
+}, (e) => {
+  console.log('Unable to create User');
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved Todo', doc);
+// }, (e) => {
+//   console.log('Unable to save Todo');
+// });
+
+// var newTodo = new Todo({
+//   text: 'Walk the bird',
+//
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved Todo', doc);
+// }, (e) => {
+//   console.log('Unable to save Todo');
+// });
