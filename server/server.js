415129daab5c3941b18541244482572ac2667f26
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-11-19 11:58:06 -0500

    get individual resource by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb').ObjectID;
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -31,6 +32,20 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send({todo});
+    } else {
+      res.status(404).send();
+    }
+  }).catch((e) => res.status(400).send());
 });
 
 module.exports = {app};
