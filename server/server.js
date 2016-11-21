e21960264f0fc3b5c4b72a52c579c77df1aa4b53
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-11-21 13:39:54 -0500

    test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -59,39 +59,8 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       return res.send(404);
     }
-    res.status(200).send(todo);
+    res.status(200).send({todo: todo});
   }).catch((e) => res.status(400).send());
 });
 
 module.exports = {app};
-
-// var newUser = new User({
-//   email: 'len@test.com'
-// });
-//
-// newUser.save().then((res) => {
-//   console.log('User created', res);
-// }, (e) => {
-//   console.log('Unable to create User');
-// });
-
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved Todo', doc);
-// }, (e) => {
-//   console.log('Unable to save Todo');
-// });
-
-// var newTodo = new Todo({
-//   text: 'Walk the bird',
-//
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved Todo', doc);
-// }, (e) => {
-//   console.log('Unable to save Todo');
-// });
