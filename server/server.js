7c1631ce8e1e6b30ecfbdf6de4917256537c51d9
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-11-21 17:06:31 -0500

    add POST /users route

--- server/server.js
+++ server/server.js
@@ -92,4 +92,16 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+//POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => res.status(400).send(e));
+});
+
+
+
 module.exports = {app};
