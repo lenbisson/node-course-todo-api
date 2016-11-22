e994a00aeabbad64977d2ac7841751f0f82ea7a8
Len Bisson <leonard_bisson@unitedsolutions.biz>
2016-11-22 14:02:50 -0500

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,25 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
 
-var data = {
-  id: 10
-};
-
-var token = jwt.sign(data, '123abc');
-console.log(token);
-
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded)
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded)
 
 // var message = 'I am user number 1';
 //
